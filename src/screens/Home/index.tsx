52e6ba89c01a9605c4103526df7207f90ca116ca
andre macedo <andrevitoreur@gmail.com>
2023-08-27 20:51:57 +0200

    Preparing Environment

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { View,Text } from "react-native";
+import React from 'react';
+export default function Home (){
+    return(
+        <View className="bg-blue-500 flex-1 items-center justify-center">
+            <Text className="font-bold text-2xl">Home</Text>
+        </View>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app.{js,jsx,ts,tsx}", 
+  "./src/screens//**/*.{ts,tsx}",
+  "./src/components//**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
